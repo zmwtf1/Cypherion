6ad09f9cb17347cb773c1cfc6dcd2922f0765f3f
ZMWTF <zmwtf1@hotmail.com>
2025-09-01 19:29:10 -0400

    Update speedandjump.lua

--- Scripts/speedandjump.lua
+++ Scripts/speedandjump.lua
@@ -1,8 +1,8 @@
--- Define Ts
+-- Define
 local players = game:GetService("Players")
 local player = players.LocalPlayer
 local speedandjumpthing = player.Character.Humanoid
 
--- Set TS
+-- Set
 speedandjumpthing.WalkSpeed = 50
 speedandjumpthing.JumpPower = 150
